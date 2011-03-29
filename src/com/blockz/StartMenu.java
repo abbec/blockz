38913c0b5faf89ad6299cc67da8a319aecba56fa
Albert Cervin <albert@nat0.se>
2011-03-29 11:13:12 +0200

    Changed the most vital comment ever.

--- src/com/blockz/StartMenu.java
+++ src/com/blockz/StartMenu.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class StartMenu extends Activity {
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. Hej. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
