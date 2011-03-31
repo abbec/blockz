20917829aeacc2dc5e76d7ebfd3a453ec2178bcd
Lovisa Johansson <lajjtis@gmail.com>
2011-03-31 17:02:08 +0200

    Fixat med spriteklasserna.

---
+++ src/com/blockz/graphics/Sprite.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.blockz.graphics;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.blockz.R;
+
+
+
+/**
+ * @author 
+ *
+ */
+
+public abstract class Sprite 
+{
+	public abstract void draw(int x, int y);	
+		
+}
