6c88bea5e64fb3c6220c0e31cf236ac32f154ee2
GL, FL, AC <GL, FL, AC@localhost>
2011-03-31 16:38:49 +0200

    Created Scene and merged SpriteManager with Scene

--- src/com/blockz/graphics/Scene.java
+++ src/com/blockz/graphics/Scene.java
@@ -3,11 +3,47 @@
  */
 package com.blockz.graphics;
 
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.Window;
+
 /**
  * @author
  *
  */
-public class Scene 
+public class Scene extends SurfaceView
 {
-
+	private Map<String,Sprite> _spriteTable;
+	
+    public Scene(Context context)
+    {
+    	super(context);
+    	_spriteTable = new Map<String,Sprite>();
+    }
+    
+    /**
+	 * addSprite() takes a sprite name as argument and add the corresponding sprite object to the MAP.
+	 */	
+	public void addSprite(String spriteName)
+	{
+		if(_spriteTable.get(spriteName) != null)
+			_spriteTable.put(spriteName, new Sprite(spriteName + ".png"));
+	}
+    
+	
+    public void draw(List<Item> renderList)
+    {
+    	for(int i=0; i<renderList.size(); i++)
+    	{
+    		Item it = renderList.get(i);
+    		Sprite s = _spriteTable.get(it.getSpriteName() + ".png");
+    		s.draw();
+    		
+    	}
+    }
+    
 }
