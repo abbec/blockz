4d36837a58c3a3c7c1a67a1aed44ae506d055c72
GL, FL, AC <GL, FL, AC@localhost>
2011-03-31 16:58:01 +0200

    Added SurfaceHolder to Scene.

--- src/com/blockz/graphics/Scene.java
+++ src/com/blockz/graphics/Scene.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.Window;
 
@@ -18,10 +19,12 @@ import android.view.Window;
 public class Scene extends SurfaceView
 {
 	private Map<String,Sprite> _spriteTable;
+	private SurfaceHolder _surfHolder;
 	
     public Scene(Context context)
     {
     	super(context);
+    	_surfHolder = getHolder();
     	_spriteTable = new Map<String,Sprite>();
     }
     
@@ -29,7 +32,7 @@ public class Scene extends SurfaceView
 	 * addSprite() takes a sprite name as argument and add the corresponding sprite object to the MAP.
 	 */	
 	public void addSprite(String spriteName)
-	{
+	{		
 		if(_spriteTable.get(spriteName) != null)
 			_spriteTable.put(spriteName, new Sprite(spriteName + ".png"));
 	}
@@ -41,7 +44,7 @@ public class Scene extends SurfaceView
     	{
     		Item it = renderList.get(i);
     		Sprite s = _spriteTable.get(it.getSpriteName() + ".png");
-    		s.draw();
+    		s.draw(/*Position + canvas?*/);
     		
     	}
     }
