c5fa3564e6124a025b8286059b28399b8704a711
GL, EA <GL, EA@localhost>
2011-04-05 11:16:52 +0200

    Skapade Block och ?ndrade _type i Item till heltal ist?llet f?r string.

---
+++ src/com/blockz/logic/Block.java
@@ -0,0 +1,51 @@
+package com.blockz.logic;
+import com.blockz.logic.Item;
+
+
+public abstract class Block extends Item 
+{
+	
+	public boolean _movable;
+	
+	
+	public class FixedBlock extends Block
+	{
+		public FixedBlock()
+		{
+			_movable = false;
+		}
+		
+		public void move(Coordinate c)
+		{
+			 setPosition(c);
+		}
+		
+	}
+	
+	public class MovableBlock extends Block
+	{
+		private int _direction;
+		private static final int UP = 0;
+		private static final int RIGHT = 1;
+		private static final int DOWN = 2;
+		private static final int LEFT = 3;
+		
+		public MovableBlock()
+		{
+			_movable = true;
+		}
+		
+		public void move(int dir)
+		{
+			_direction = dir;
+			/*
+			 *Måste räkna ut slutpositionen för blocket på något sätt. Startposition har man i _position, 
+			 *riktning i _direction. Kolla level för att se var blocket tar vägen. 
+			 */
+		}
+		
+		
+		
+	}
+	
+}
