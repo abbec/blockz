2251881e036585d79eb042828afc8c8d8532e3a9
AP, CC, EA <AP, CC, EA@localhost>
2011-04-05 16:44:42 +0200

    added coordinate scale in Level

--- src/com/blockz/logic/Level.java
+++ src/com/blockz/logic/Level.java
@@ -21,19 +21,30 @@ import com.blockz.graphics.Scene;
  */
 public class Level 
 {
+	public final static int STONE_FIXED = -13421773;
+	public final static int STONE_MOVABLE = -6710887;
+	public final static int HUD = -16777216;
+	public final static int GRASS = -3355444;
+	public final static int WATER = -5066052;
+	public final static int GOAL = -1;
+	public final static int START = -1710619;
+	
+	
 	private LinkedList<Item> _itemList;
 	private ConcurrentLinkedQueue<Item> _renderQueue;
 	private Scene _scene;
 	private Bitmap _levelImage;
 	private Context _context;
+	private int _width,_height = 0;
 	
-	public Level(Context context, Scene theScene)
+	public Level(Context context, Scene theScene, int width, int height)
 	{
 		_itemList = new LinkedList<Item>();
 		_renderQueue = new ConcurrentLinkedQueue<Item>();
 		_context = context;
 		_scene = theScene;
-
+		this._width = width / 12;
+		this._height = height / 8;
 	}
 	
     /**
@@ -46,8 +57,8 @@ public class Level
 		_scene.draw(_renderQueue);
 	}
     /**
-	 * readLevel() uses the LevelReader to set _itemList
-	 * @param 
+	 * readLevel() reads a level as resource and populates the _itemList and _renderQueue.
+	 * @param int with the resource number to a level
 	 */
 	public void readLevel(int resourceNumber)
 	{
@@ -58,31 +69,78 @@ public class Level
 		{
 			for(int row = 0; row<_levelImage.getWidth(); row++)
 			{
-				 int pixelValue =_levelImage.getPixel(row,col);			 
-				 /*int drawableValue =-1;
-				 int staticInt =-1; 
+				 int pixelValue =_levelImage.getPixel(row,col);
+				 String ps = java.lang.Integer.toHexString(pixelValue);
+				 System.out.println(pixelValue +" : "+ps );
+				 int drawableValue =-1;
+				 int staticInt =-1;
+				 boolean isBlockMovable = false;
 				 switch (pixelValue) {
-		            case -3355444:  drawableValue =  R.drawable.grass; 	staticInt =	Scene.STATIC_SPRITE;	break;
-		            case 2:  drawableValue =  R.drawable.grasshole; staticInt =	Scene.STATIC_SPRITE;		break;
-		            case 3:  drawableValue =  R.drawable.stone; 	staticInt =	Scene.STATIC_SPRITE;		break;
-		            case 4:  drawableValue =  R.drawable.gubbe; 	staticInt =	Scene.ANIMATED_SPRITE;		break;
-		            case 5:  drawableValue =  R.drawable.water; 	staticInt =	Scene.STATIC_SPRITE;		break;
-		            default: drawableValue =  R.drawable.icon; 		staticInt =	Scene.STATIC_SPRITE;		break;
+					case GRASS:
+						drawableValue =  R.drawable.grass;
+						staticInt = Scene.STATIC_SPRITE;
+						isBlockMovable = false;
+						break;
+					case STONE_FIXED:
+						drawableValue =  R.drawable.stone;
+						staticInt =	Scene.STATIC_SPRITE;
+						isBlockMovable = false;
+						break;
+					case STONE_MOVABLE:
+						drawableValue =  R.drawable.stone;
+						staticInt =	Scene.STATIC_SPRITE;
+						isBlockMovable = true;
+						break;
+					case WATER:
+						drawableValue =  R.drawable.water;
+						staticInt =	Scene.STATIC_SPRITE;
+						isBlockMovable = false;
+						break;
+					case GOAL:
+						drawableValue =  R.drawable.goal;
+						staticInt =	Scene.STATIC_SPRITE;
+						isBlockMovable = false;
+						break;
+					case START:	
+						drawableValue =  R.drawable.grasshole;
+						staticInt =	Scene.STATIC_SPRITE;
+						isBlockMovable = false;
+						break;
+					case HUD:
+						drawableValue =  R.drawable.water;
+						staticInt =	Scene.STATIC_SPRITE;
+						isBlockMovable = false;
+						break;
+		            default:
+		            	drawableValue =  R.drawable.icon;
+		            	staticInt =	Scene.STATIC_SPRITE;
+		            	isBlockMovable = false;
+		            	break;
 		        }
 
 				_scene.addSprite(drawableValue, staticInt);
-				if(staticInt == Scene.STATIC_SPRITE)
+				if(!isBlockMovable)
 				{
-					_itemList.add(new FixedBlock(new Coordinate(row, col), drawableValue));
+					WallBlock f = new WallBlock(new Coordinate(row * _height, col * _width), drawableValue);
+					_itemList.add(f);
+					_renderQueue.add(f);
+					 System.out.println("STATIC BLOCK ADDED" );
 				}
-				else if(staticInt == Scene.ANIMATED_SPRITE)
+				else if(isBlockMovable)
 				{
-					_itemList.add(new MovableBlock(new Coordinate(row, col), drawableValue));
+					MovableBlock m = new MovableBlock(new Coordinate(row * _height, col * _width), drawableValue);
+					_itemList.add(m);
+					_renderQueue.add(m);
+					 System.out.println("MOVABLE BLOCK ADDED" );
 				}
 				else
 				{
 					Assert.assertTrue("Level Class: Wrong Movable constant",false);
-				}*/
+				}
+				
+				System.out.println("Width: "+_width);
+				System.out.println("Height: "+_height);
+
 			}
 		}
 		
