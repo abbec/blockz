1e44614b89777a7f9ec63b8d56e35c9fee335825
matnix <matnix@localhost>
2011-04-05 11:57:12 +0200

    Added a class for animated sprites and fixed static sprites

--- gen/com/blockz/R.java
+++ gen/com/blockz/R.java
@@ -11,12 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int grass=0x7f020000;
-        public static final int grasshole=0x7f020001;
-        public static final int gubbe=0x7f020002;
-        public static final int icon=0x7f020003;
-        public static final int stone=0x7f020004;
-        public static final int water=0x7f020005;
+        public static final int arrow=0x7f020000;
+        public static final int grass=0x7f020001;
+        public static final int grasshole=0x7f020002;
+        public static final int gubbe=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int sprites=0x7f020005;
+        public static final int stone=0x7f020006;
+        public static final int water=0x7f020007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
---
+++ src/com/blockz/graphics/AnimatedSprites.java
@@ -0,0 +1,68 @@
+package com.blockz.graphics;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+public class AnimatedSprites extends Sprite
+{
+	
+	private Bitmap _sprite;
+    private int _xPos;
+    private int _yPos;
+    private Rect _sRectangle;
+    private int _FPS = 5;
+    private int _noOfFrames;
+    private int _currentFrame;
+    private long _frameTimer;
+    private int _spriteHeight = 72;
+    private int _spriteWidth = 41;
+    private int _theFrameCount = 4;
+    private Context _context;
+    
+    public AnimatedSprites(int typeID, Context context)
+    {
+    	this._context = context;
+		_sprite = BitmapFactory.decodeResource(_context.getResources(), typeID);
+    	_sRectangle = new Rect(0,0,0,0);
+        _frameTimer =0;
+        _currentFrame =0;
+        _xPos = 80;
+        _yPos = 200;
+        _sRectangle.top = 0;
+        _sRectangle.bottom = _spriteHeight;
+        _sRectangle.left = 0;
+        _sRectangle.right = _spriteWidth;
+        _FPS = 1000 /_FPS;
+        _noOfFrames = _theFrameCount;
+    	
+    }
+    
+    
+    public void Update(long gameTime) 
+    {
+        if(gameTime > _frameTimer + _FPS ) {
+            _frameTimer = gameTime;
+            _currentFrame +=1;
+     
+            if(_currentFrame >= _noOfFrames) {
+                _currentFrame = 0;
+            }
+        }
+     
+        _sRectangle.left = _currentFrame * _spriteWidth;
+        _sRectangle.right = _sRectangle.left + _spriteWidth;
+    }
+    
+    public void draw(Canvas canvas, int x, int y) 
+    {
+        Rect dest = new Rect(_xPos, _yPos, _xPos + _spriteWidth,
+                        _yPos + _spriteHeight);
+     
+        canvas.drawBitmap(_sprite, _sRectangle, dest, null);
+        
+    }    
+
+}
