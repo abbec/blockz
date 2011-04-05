0ac91f7524e267187d1714285329c6c9f5e300bb
AP, CC <AP, CC@localhost>
2011-04-05 13:35:13 +0200

    Added FileReader in Level Class and Defaultlevel

--- gen/com/blockz/R.java
+++ gen/com/blockz/R.java
@@ -12,14 +12,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int arrow=0x7f020000;
-        public static final int goal=0x7f020001;
-        public static final int grass=0x7f020002;
-        public static final int grasshole=0x7f020003;
-        public static final int gubbe=0x7f020004;
-        public static final int icon=0x7f020005;
-        public static final int sprites=0x7f020006;
-        public static final int stone=0x7f020007;
-        public static final int water=0x7f020008;
+        public static final int defaultlevel=0x7f020001;
+        public static final int goal=0x7f020002;
+        public static final int grass=0x7f020003;
+        public static final int grasshole=0x7f020004;
+        public static final int gubbe=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int sprites=0x7f020007;
+        public static final int stone=0x7f020008;
+        public static final int water=0x7f020009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
