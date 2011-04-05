b94ac2f9afb9ee145daf80314a89b98b8666af1f
AC, FL <AC, FL@localhost>
2011-04-05 09:22:41 +0200

    Modified Scene class to fit Sprite rendering.

--- src/com/blockz/graphics/Scene.java
+++ src/com/blockz/graphics/Scene.java
@@ -3,10 +3,12 @@
  */
 package com.blockz.graphics;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import junit.framework.Assert;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -18,35 +20,57 @@ import android.view.Window;
  */
 public class Scene extends SurfaceView
 {
-	private Map<String,Sprite> _spriteTable;
+	public final static int STATIC_SPRITE = 0;
+	public final static int ANIMATED_SPRITE = 1;
+	
+	private TreeMap<Integer, Sprite> _spriteTable;
 	private SurfaceHolder _surfHolder;
 	
     public Scene(Context context)
     {
     	super(context);
     	_surfHolder = getHolder();
-    	_spriteTable = new Map<String,Sprite>();
+    	_spriteTable = new TreeMap<Integer, Sprite>();
     }
     
     /**
-	 * addSprite() takes a sprite name as argument and add the corresponding sprite object to the MAP.
+	 * addSprite() takes a spriteId as argument and add the corresponding sprite object to the MAP.
+	 * 
+	 * @param type The type of sprite: STATIC_SPRITE = 0, ANIMATED_SPRITE = 1
 	 */	
-	public void addSprite(String spriteName)
+	public void addSprite(int spriteId, int type)
 	{		
-		if(_spriteTable.get(spriteName) != null)
-			_spriteTable.put(spriteName, new Sprite(spriteName + ".png"));
+		if(_spriteTable.get(spriteId) != null)
+		{
+			if (type == STATIC_SPRITE)
+				_spriteTable.put(spriteId, new StaticSprite(spriteId));
+			else if (type == ANIMATED_SPRITE)
+				_spriteTable.put(spriteId, new AnimatedSprite(spriteId));
+			else
+				Assert.assertTrue("This sprite type does not exist!", false);
+		}
+			
 	}
     
-	
-    public void draw(List<Item> renderList)
+	/**
+	 * draw() locks the canvas, draws the Items in the parameter Queue<Item> list and then unlocks the canvas.
+	 * 
+	 * @param renderList of type Queue<Item> 
+	 */
+    public void draw(Queue<Item> renderList)
     {
-    	for(int i=0; i<renderList.size(); i++)
+    	// Lock the canvas to begin editing its pixels
+    	Canvas c = _surfHolder.lockCanvas();
+    	
+    	while(!renderList.empty())
     	{
-    		Item it = renderList.get(i);
+    		Item it = renderList.pop();
     		Sprite s = _spriteTable.get(it.getSpriteName() + ".png");
-    		s.draw(/*Position + canvas?*/);
-    		
+    		s.draw(c, it.getX(), it.getY());
     	}
+    	
+    	// Unlock the canvas to show the screen
+    	_surfHolder.unlockCanvasAndPost(c);
     }
     
 }
