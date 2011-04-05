80a1530d5409a78b6830896c868b356aaaf308fa
AP, CC <AP, CC@localhost>
2011-04-05 13:35:13 +0200

    Added FileReader in Level Class and Defaultlevel

--- src/com/blockz/logic/Level.java
+++ src/com/blockz/logic/Level.java
@@ -5,8 +5,17 @@ package com.blockz.logic;
 
 import java.util.List;
 import java.util.Queue;
+
 import junit.framework.Assert;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.blockz.R;
 import com.blockz.graphics.Scene;
+import com.blockz.logic.Block.FixedBlock;
+import com.blockz.logic.Block.MovableBlock;
+import com.blockz.logic.Item.Coordinate;
 
 /**
  * @author 
@@ -14,14 +23,17 @@ import com.blockz.graphics.Scene;
  */
 public class Level 
 {
-	List<Item> _itemList;
-	Queue<Item> _renderQueue;
-	Scene _scene;
+	private List<Item> _itemList;
+	private Queue<Item> _renderQueue;
+	private Scene _scene;
+	private Bitmap _levelImage;
+	private Context _context;
 	
-	public Level()
+	public Level(Context context)
 	{
 		_itemList = new List<Item>();
 		_renderQueue = new Queue<Item>();
+		_context = context;
 	}
 	
     /**
@@ -29,7 +41,7 @@ public class Level
 	 */	
 	public void render()
 	{
-		if(_scene==NULL)
+		if(_scene==null)
 			Assert.assertTrue("Level Class: No scene is set", false);
 		_scene.draw(_renderQueue);
 	}
@@ -47,9 +59,41 @@ public class Level
 	 * readLevel() uses the LevelReader to set _itemList
 	 * @param 
 	 */
-	public void readLevel(/*Get information about which level to read from Game*/)
+	public void readLevel(/* GET LVL NR FROM GAME*/)
 	{
-		//call levelreader fill _itemList
+		_levelImage = BitmapFactory.decodeResource(_context, R.drawable.grass);
+		for(int row = 0; row<_levelImage.getHeight(); row++)
+		{
+			for(int col = 0; col<_levelImage.getWidth(); row++)
+			{
+				 int pixelValue =_levelImage.getPixel(row,col);
+				 int drawableValue =-1;
+				 int staticInt =-1; 
+				 switch (pixelValue) {
+		            case 1:  drawableValue =  R.drawable.grass; 	staticInt =	Scene.STATIC_SPRITE;		break;
+		            case 2:  drawableValue =  R.drawable.grasshole; staticInt =	Scene.STATIC_SPRITE;		break;
+		            case 3:  drawableValue =  R.drawable.stone; 	staticInt =	Scene.STATIC_SPRITE;		break;
+		            case 4:  drawableValue =  R.drawable.gubbe; 	staticInt =	Scene.ANIMATED_SPRITE;		break;
+		            case 5:  drawableValue =  R.drawable.water; 	staticInt =	Scene.STATIC_SPRITE;		break;
+		            default: drawableValue =  R.drawable.icon; 		staticInt =	Scene.STATIC_SPRITE;		break;
+		        }
+
+				_scene.addSprite(drawableValue, staticInt);
+				if(staticInt == Scene.STATIC_SPRITE)
+				{
+					_itemList.add(new FixedBlock(new Coordinate(row, col), drawableValue));
+				}
+				else if(staticInt == Scene.ANIMATED_SPRITE)
+				{
+					_itemList.add(new MovableBlock(new Coordinate(row, col), drawableValue));
+				}
+				else
+				{
+					Assert.assertTrue("Level Class: Wrong Movable constant",false);
+				}
+			}
+		}
+		
 	}
 	
 }
