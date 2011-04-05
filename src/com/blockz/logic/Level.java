deefe305ad021c9879d30da10c2d37bf900ea9f7
AP, CC <AP, CC@localhost>
2011-04-05 09:24:08 +0200

    Basic structure of Level Class

--- src/com/blockz/logic/Level.java
+++ src/com/blockz/logic/Level.java
@@ -3,11 +3,44 @@
  */
 package com.blockz.logic;
 
+import java.util.List;
+import java.util.Queue;
+
+import junit.framework.Assert;
+
+import com.blockz.graphics.Scene;
+
 /**
  * @author 
  *
  */
 public class Level 
 {
-
+	List<Item> _itemList;
+	Queue<Item> _renderQueue;
+	Scene _scene;
+	
+	public Level()
+	{
+		_itemList = new List<Item>();
+		_renderQueue = new Queue<Item>();
+	
+	}
+	public void render()
+	{
+		if(_scene==NULL)
+			Assert.assertTrue("Level Class: No scene is set", false);
+		_scene.draw(_renderQueue);
+	}
+	
+	public void setScene(Scene theScene)
+	{
+		_scene = theScene;
+	}
+	
+	public void readLevel()
+	{
+		//call levelreader fill _itemList
+	}
+	
 }
