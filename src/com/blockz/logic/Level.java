05cc2decb24da1adb419f6acbd3eb47cf10fe4cf
AP, CC <AP, CC@localhost>
2011-04-05 09:38:57 +0200

    Comments in Level

--- src/com/blockz/logic/Level.java
+++ src/com/blockz/logic/Level.java
@@ -5,14 +5,12 @@ package com.blockz.logic;
 
 import java.util.List;
 import java.util.Queue;
-
 import junit.framework.Assert;
-
 import com.blockz.graphics.Scene;
 
 /**
  * @author 
- *
+ * 
  */
 public class Level 
 {
@@ -24,8 +22,11 @@ public class Level
 	{
 		_itemList = new List<Item>();
 		_renderQueue = new Queue<Item>();
-	
 	}
+	
+    /**
+	 * render() calls the draw() function in the Scene Class with the _renderQueue as argument.
+	 */	
 	public void render()
 	{
 		if(_scene==NULL)
@@ -33,12 +34,20 @@ public class Level
 		_scene.draw(_renderQueue);
 	}
 	
+    /**
+	 * setScene() initiates the _scene
+	 * @param theScene of type Scene
+	 */		
 	public void setScene(Scene theScene)
 	{
 		_scene = theScene;
 	}
-	
-	public void readLevel()
+
+    /**
+	 * readLevel() uses the LevelReader to set _itemList
+	 * @param 
+	 */
+	public void readLevel(/*Get information about which level to read from Game*/)
 	{
 		//call levelreader fill _itemList
 	}
