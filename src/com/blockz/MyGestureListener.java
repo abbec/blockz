5f430308de04bf0eb1ef1334940243ad6e33fcf8
matnix <matnix@localhost>
2011-04-12 11:56:45 +0200

    MyEvent added and currentEvent in level

--- src/com/blockz/MyGestureListener.java
+++ src/com/blockz/MyGestureListener.java
@@ -1,4 +1,6 @@
 package com.blockz;
+import com.blockz.logic.Coordinate;
+
 import android.view.MotionEvent;
 import android.view.GestureDetector.SimpleOnGestureListener;
 
@@ -9,10 +11,23 @@ public class MyGestureListener extends SimpleOnGestureListener
 	private static final int SWIPE_MAX_OFF_PATH = 100;
 	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 
+	private static final int LEFT = 0;
+	private static final int RIGHT =1;
+	private static final int UP = 2;
+	private static final int DOWN = 3;
+	private MyEvent _event;
+	
+	public MyGestureListener(MyEvent e)
+	{
+		_event = e;
+	}
+	
 	
 	@Override
 	public boolean onSingleTapUp(MotionEvent e)
 	{
+		_event.setCoordinate(new Coordinate((int)e.getRawX(), (int)e.getRawY()));
+		
 		System.out.println("Press!");
 		return true;
 	}
@@ -20,11 +35,13 @@ public class MyGestureListener extends SimpleOnGestureListener
 	@Override
 	public void onLongPress(MotionEvent e)
 	{
+		_event.setCoordinate(new Coordinate((int)e.getRawX(), (int)e.getRawY()));
 		System.out.println("Long Press!");
 	}
 	@Override
 	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) 
 	{
+		_event.setCoordinate(new Coordinate((int)e1.getRawX(), (int)e1.getRawY()));
 		System.out.println("Offset : " + Math.abs(e1.getY() - e2.getY()));
 		
 		float offPathX = Math.abs(e1.getX() - e2.getX());
@@ -33,21 +50,26 @@ public class MyGestureListener extends SimpleOnGestureListener
 		if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && offPathY < SWIPE_MAX_OFF_PATH && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) 
 		{
 			System.out.println("Fling Left!");
+			
+			_event.setDirection(LEFT);
 			return true;
 		} 
 		else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && offPathY < SWIPE_MAX_OFF_PATH && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) 
 		{
 			System.out.println("Fling Right!");
+			_event.setDirection(RIGHT);
 			return true;
 		}
 		else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && offPathX < SWIPE_MAX_OFF_PATH && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) 
 		{
 			System.out.println("Fling Down!");
+			_event.setDirection(DOWN);
 			return true;
 		}
 		else if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && offPathX < SWIPE_MAX_OFF_PATH && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) 
 		{
 			System.out.println("Fling Up!");
+			_event.setDirection(UP);
 			return true;
 		}
 		
