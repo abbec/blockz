bb5e9536a54c6a5e47f6d8d95361b019ace03749
matnix <matnix@localhost>
2011-04-12 11:57:52 +0200

    MyEvent

---
+++ src/com/blockz/MyEvent.java
@@ -0,0 +1,23 @@
+package com.blockz;
+
+import com.blockz.logic.Coordinate;
+
+public class MyEvent {
+	private int _direction;
+	private Coordinate _coordinate;
+	
+	public int getDirection() {
+		return _direction;
+	}
+	public void setDirection(int direction) {
+		_direction = direction;
+	}
+	public Coordinate getCoordinate() {
+		return _coordinate;
+	}
+	public void setCoordinate(Coordinate coordinate) {
+		_coordinate = coordinate;
+	} 
+	
+
+}
