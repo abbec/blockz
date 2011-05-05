1a0498aba724658a5e25c7ce033b002fadbbbd6d
EA,CC <EA,CC@localhost>
2011-05-05 11:20:01 +0200

    Dev Features in Da Huud

--- src/com/blockz/GameThread.java
+++ src/com/blockz/GameThread.java
@@ -3,6 +3,7 @@
  */
 package com.blockz;
 
+import android.os.Debug;
 import android.util.Log;
 
 /**
@@ -13,11 +14,9 @@ public class GameThread extends Thread
 {
 	public final static int RUNNING = 0;
 	public final static int PAUSED = 1;
-	
-	
     private Game _game;
     private boolean _run;
-    
+    private Debug.MemoryInfo _devInfo;
     private int _state;
     
     public static final int UPDATE_RATE = 30;
@@ -58,7 +57,11 @@ public class GameThread extends Thread
     public void run() 
     {	
     	int min_frame_time = 1000/UPDATE_RATE;
+    	int fpsCounter = 0;
     	long frameTime = 0;
+    	long devFrameTime = 0;
+    	long renderTime = 0;
+    	_devInfo = new Debug.MemoryInfo();
     	
     	Log.d("B_INFO", "Starting main loop");
     	
@@ -70,13 +73,28 @@ public class GameThread extends Thread
             	{
             		// Update the level
             		_game.getLevel().update(_game.gameTime());
-            		
-            		frameTime = _game.gameTime();
+            		frameTime = _game.gameTime();	
             	}
             	
             	// Render each frame so that we get cool FPS
            		_game.getLevel().render(_game.gameTime());
             	
+           		
+           		//Dev Tools, sends FPS and Memory Usage to the HUD
+           		if(_game.gameTime() - renderTime > 1000)
+           		{
+           			Debug.getMemoryInfo(_devInfo);       		
+           			long privateMemory = _devInfo.getTotalPrivateDirty();
+           			long totalMemory = _devInfo.getTotalPss();     			
+           			renderTime = _game.gameTime();
+           			_game.getHud().clearDevString();
+           			_game.getHud().appendDevString("FPS: " + fpsCounter +"\n");
+           			_game.getHud().appendDevString("Memory: " + privateMemory/totalMemory + "%\n");
+
+           			fpsCounter = 0;
+           		}
+           		else
+           			fpsCounter++;
         	}
         	   	
         }
